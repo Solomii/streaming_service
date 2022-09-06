862befb8db8da6850897a421e70da2f4ca61d089
Solomii <kushniryksolomiya@gmail.com>
2022-09-06 15:45:54 +0300

    [#0 +] Found all users

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,26 @@ const User = require("../models/User");
 const CryptoJS = require("crypto-js");
 const verify = require("../verifyToken")
 
+router.get('/', verify, async (req, res) => {
+  try {
+   const users =  await User.find();
+   res.status(200).json(users);
+    
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.get("/find/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...info } = user._doc;
+    res.status(200).json(info)
+  } catch (error) {
+    res.status(500).json(error);
+  }
+})
+
 router.put('/:id', verify, async (req, res) => {
   if (req.user.id === req.params.id || req.user.isAdmin) {
     if (req.user.password) {
@@ -21,6 +41,20 @@ router.put('/:id', verify, async (req, res) => {
   }
   } else {
     res.status(403).json("you can update your account")
+ }
+});
+
+router.delete('/:id', verify, async (req, res) => {
+  if (req.user.id === req.params.id || req.user.isAdmin) {
+  try {
+   await User.findByIdAndDelete(req.params.id), 
+   res.status(200).json("User deleted");
+    
+  } catch (error) {
+    res.status(500).json(error);
+  }
+  } else {
+    res.status(403).json("you can delete your account")
  }
 });
 
